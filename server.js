fe4967c75e0e76fec98068638841a0e05562904d
JP Whitaker <jpw@hawaii.edu>
2018-04-19 10:31:15 -1000

    reordered functions

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ var express = require('express');
 var socket = require("socket.io");
 var app = express();
 
-//triggered by phone going to site
-//sends take pic signal via sockets
-//tessel sends back pic and we save to disk
-//respond to request after save
+//1. triggered by phone going to site
+//2. sends take pic signal via sockets
+//3. tessel sends back pic and we save to disk
+//4. respond to request by sending image after save
 
 var globalSocket;
 var globalRes;
 
+var takePicture = () => {
+  globalSocket.emit('takePicture', true);
+}
+
 var server = app.listen(4000, function(){
   console.log('listening to requests on port 4000')
+})
+
+//socket setup
+var io = socket(server);
+io.on('connection', function(socket){
+  globalSocket = socket;
+  console.log(`Made socket connection to id ${socket.id}`)
 })
 
 app.get('/', function (req, res) {
@@ -21,24 +32,11 @@ app.get('/', function (req, res) {
   takePicture();
 })
 
-//socket setup
-var io = socket(server);
-
-io.on('connection', function(socket){
-  globalSocket = socket;
-  console.log(`Made socket connection to id ${socket.id}`)
-})
-
-var takePicture = () => {
-  globalSocket.emit('takePicture', true);
-}
-
 app.post('/upload', function(request, res) {
   console.log('saving pic');
   var fileName = __dirname + `/public/${Date.now()}.jpg`;
   request.pipe(fs.createWriteStream(__dirname + `/public/${Date.now()}.jpg`))
   .on('finish', function(){
-    console.log("ENDING");
     globalRes.sendFile(fileName);
     res.sendStatus(200);
     console.timeEnd('get/');
